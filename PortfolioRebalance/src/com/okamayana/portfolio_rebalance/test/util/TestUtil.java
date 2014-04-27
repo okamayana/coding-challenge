6d255f3ba886aafcef17209192eb38a611f2d049
Odhita Kamayana <odhitakamayana@gmail.com>
2014-04-27 05:33:00 -0400

    Added TestUtil class for common test info

---
+++ PortfolioRebalance/src/com/okamayana/portfolio_rebalance/test/util/TestUtil.java
@@ -0,0 +1,10 @@
+package com.okamayana.portfolio_rebalance.test.util;
+
+import java.io.File;
+
+public class TestUtil {
+	
+	private static final String BASE_PATH_STRING = "test_inputs/";
+	public static final File BASE_PATH = new File(BASE_PATH_STRING);
+
+}

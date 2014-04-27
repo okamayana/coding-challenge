9cdbd11761022653279de15ce9483294d92a80b3
Odhita Kamayana <odhitakamayana@gmail.com>
2014-04-26 21:46:46 -0400

    Added static method that parses CSV input into Portfolio object

--- PortfolioRebalance/src/com/okamayana/portfolio_rebalance/Portfolio.java
+++ PortfolioRebalance/src/com/okamayana/portfolio_rebalance/Portfolio.java
@@ -1,5 +1,9 @@
 package com.okamayana.portfolio_rebalance;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,5 +31,32 @@ public class Portfolio {
 
 	public void appendInvestment(Investment investment) {
 		investments.add(investment);
+	}
+	
+	public static Portfolio fromCsv(File csvFile) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+		List<Investment> investments = new ArrayList<Investment>();
+		double totalInvestment = 0;
+
+		for (String row; (row = reader.readLine()) != null;) {
+			String[] columns = row.split(",");
+			String ticker = columns[Investment.COLUMN_TICKER];
+			double allocationTarget = Double
+					.parseDouble(columns[Investment.COLUMN_ALLOCATION_TARGET]);
+			double allocationActual = Double
+					.parseDouble(columns[Investment.COLUMN_ALLOCATION_ACTUAL]);
+			int sharesOwned = Integer
+					.parseInt(columns[Investment.COLUMN_SHARES_OWNED]);
+			double sharePrice = Double
+					.parseDouble(columns[Investment.COLUMN_SHARE_PRICE]);
+
+			Investment investment = new Investment(ticker, allocationTarget,
+					allocationActual, sharesOwned, sharePrice);
+			investments.add(investment);
+			totalInvestment += sharePrice * sharesOwned;
+		}
+
+		reader.close();
+		return new Portfolio(investments, totalInvestment);
 	}
 }
